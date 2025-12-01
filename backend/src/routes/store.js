330983c89fc9a889dde92259f8448b3b76b20089
mainakgupta33 <mainakgupta33@gmail.com>
2025-12-01 13:37:13 +0530

    added routes update and delete

--- backend/src/routes/store.js
+++ backend/src/routes/store.js
@@ -18,7 +18,7 @@ keyValueRouter.post('/',async (req,res)=>{
         }
 
         const keyValue = new KeyValue({ key, value});
-        await keyValue.save();
+        await kfeyValue.save();
 
         return res.status(201).json({ message : 'key-value pair stored successfully'});
     }catch(err){
@@ -42,22 +42,35 @@ keyValueRouter.get('/:key', async (req,res)=>{
     }
     return res.send('getting key-value pair');
 })
-keyValueRouter.put('/:key',(req,res)=>{
+keyValueRouter.put('/:key',async (req,res)=>{
+    const {key} = req.params;
+    const { value } = req.body;
 
+    if(!value){
+        return res.status(400).json({ error : '"value" is required'});
+    }
     try{
+        const keyValue = await KeyValue.findOneAndUpdate({key},{value},{ new : true});
 
+        if(!keyValue){
+            return res.status(404).json({ error : 'key not found'});
+        }
+        return res.status(200).json({ message : 'key-value updated successfully' , key : keyValue.key, value : keyValue.value  });
     }catch(err){
         res.status(500).json({message : 'Internal server error'});
     }    
-    return res.send('updating key-value pair');
 })
-keyValueRouter.delete('/:key',(req,res)=>{
+keyValueRouter.delete('/:key',async (req,res)=>{
     try{
-
+        const { key } = req.params
+        const keyValue = await KeyValue.findOneAndDelete({ key})
+        if(!keyValue){
+            return res.status(404).json({ error : 'Key not found'});
+        }
+        return res.status(204);
     }catch(err){
-        res.status(500).json({message : 'Internal server error'});
+        res.status(500).json({message : 'Internal server error'+err.stack.toString()});
     }
-    return res.send('deleting key-value pair');
 })
 
 module.exports = {
